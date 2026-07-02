ce52944166c31438326ecce030281e481b84825a
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-07-02 11:07:27 +0530

    Initial commit with required packaged installed.

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
