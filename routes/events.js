d6abf284dc3459f76fa93d36933f005b048a2f5c
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-08-03 10:01:26 +0530

    Events module fetching recents 6 events to display on homepage has been done.

--- routes/events.js
+++ routes/events.js
@@ -6,13 +6,16 @@ const {
   createEvent,
   updateEvent,
   deleteEvent,
+  getRecentEvents
 } = require("../controllers/eventsController");
 const upload = require("../middleware/uploads");
 
 router.get("/", getAllEvents);
+router.get("/recent", getRecentEvents);
 router.get("/:id", getEventsById);
 router.post("/", upload.single("imageUrl"), createEvent);
 router.put("/:id", upload.single("imageUrl"), updateEvent);
 router.delete("/:id", deleteEvent);
+
 
 module.exports = router;
